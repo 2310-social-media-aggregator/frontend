3bd113c98d91a011d92d1b748ed10b5899e27acf
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-03-23 12:40:09 -0500

    Introduce toggle functions in app to pass as props down to header

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -7,19 +7,30 @@ import NotFound from '../NotFound/NotFound';
 import Header from '../Header/Header';
 import mockdata from '../../mock-data-dana';
 import { useState } from 'react';
-import { Creator } from '../../types'
+import { Creator } from '../../types';
 
 function App() {
-  const [myCreators, setMyCreators] = useState<Creator[]>(mockdata.data)
-  const [creator, setCreator] = useState<Creator | null>(null)
+  const [myCreators, setMyCreators] = useState<Creator[]>(mockdata.data);
+  const [creator, setCreator] = useState<Creator | null>(null);
+
+  const handleToggleSavedCreators = () => {
+    console.log("Toggle saved creators");
+  };
+
+  const handleToggleAllCreators = () => {
+    console.log("Toggle all creators");
+  };
 
   //will eventually create a useEffect call here in order to setMyCreators() to the API call data
   
   return (
     <div className="App">
-      <Header />
+      <Header 
+        onToggleSavedCreators={handleToggleSavedCreators} 
+        onToggleAllCreators={handleToggleAllCreators} 
+      />
       <Routes>
-        <Route path="/" element = {<Home myCreators={myCreators}/>} />
+        <Route path="/" element={<Home myCreators={myCreators}/>} />
         <Route path='/details/:id' element={<Details />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
