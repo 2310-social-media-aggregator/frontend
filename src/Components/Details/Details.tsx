cdd77f6a30f26978809d57654a0cb76fcc57300e
MatthewShindel <matthew@shindel.com>
2024-03-24 13:49:15 -0400

    add twitch intergration, not linked to deployed webpage

--- src/Components/Details/Details.tsx
+++ src/Components/Details/Details.tsx
@@ -2,42 +2,58 @@ import './Details.css';
 import Youtube from '../Youtube/Youtube';
 import Twitch from '../Twitch/Twitch';
 import mockdata from '../../mock-data-dana';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Details() {
 
-	const [selectedButton, setSelectedButton] = useState<string>('button1'); // Initially select the first button
+	const [selectedButton, setSelectedButton] = useState<string>('youtube'); // Initially select the first button
 
 	const handleButtonClick = (buttonId: string) => {
 		setSelectedButton(buttonId);
 	};
+
+	useEffect(() => {
+		console.log("button changed to: ", selectedButton)
+	}, [selectedButton])
 
 	return (
 		<div className='details'>
 			<h2>This is the details page</h2>
 			<div className="header">
 				<button
-					className={selectedButton === 'button1' ? 'selected' : ''}
-					onClick={() => handleButtonClick('button1')}
+					className={selectedButton === 'youtube' ? 'selected' : ''}
+					onClick={() => handleButtonClick('youtube')}
 				>
-					Button 1
+					Youtube
 				</button>
 				<button
-					className={selectedButton === 'button2' ? 'selected' : ''}
-					onClick={() => handleButtonClick('button2')}
+					className={selectedButton === 'twitch' ? 'selected' : ''}
+					onClick={() => handleButtonClick('twitch')}
 				>
-					Button 2
+					Twitch
 				</button>
 				<button
 					className={selectedButton === 'button3' ? 'selected' : ''}
 					onClick={() => handleButtonClick('button3')}
 				>
-					Button 3
+					Etc.
 				</button>
 			</div>
-			{mockdata.data[0].attributes.youtube_videos.map(video => (
-        <Youtube key={video.id} videoId={video.id} />
-      ))}
+
+			{selectedButton === 'youtube' && (
+				<>
+					{mockdata.data[0].attributes.youtube_videos.map(video => (
+						<Youtube key={video.id} videoId={video.id} />
+					))}
+				</>
+			)}
+			{selectedButton === 'twitch' && (
+				<>
+					{mockdata.data[1].attributes.youtube_videos.map(video => (
+						<Twitch key={video.id} videoId={video.id} />
+					))}
+				</>
+			)}
 
 
 		</div>
--- src/Components/Twitch/Twitch.tsx
+++ src/Components/Twitch/Twitch.tsx
@@ -1,9 +1,24 @@
 import './Twitch.css';
+import React from 'react';
 
-function Twitch() {
-  return (
-    <></>
-  );
+interface TwtichProps {
+	videoId: string;
+}
+
+const Twitch: React.FC<TwtichProps> = ({ videoId }) => {
+	// Construct the URL of the embedded YouTube video
+	// const youtubeUrl = `https://www.youtube.com/embed/${videoId}`; will use later with actual twtich data
+
+	return (
+		<div className='twitch_card'>
+			<iframe src="https://player.twitch.tv/?channel=esl_dota2earth&parent=localhost:3000"
+				allowFullScreen
+				scrolling="no"
+				height="378"
+				width="620"></iframe>
+		</div>
+	);
 };
 
 export default Twitch;
+
