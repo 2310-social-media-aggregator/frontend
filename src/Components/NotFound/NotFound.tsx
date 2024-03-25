08fe27cd81b24e9ac76c1c056f3e66fbb1eb582d
Dana Zack <danaezack@gmail.com>
2024-03-25 13:45:29 -0600

    Feat: add Cypress testing for NotFound page

--- src/Components/NotFound/NotFound.tsx
+++ src/Components/NotFound/NotFound.tsx
@@ -5,7 +5,7 @@ function NotFound() {
   return (
     <section className='not-found-container'>
       <h2 className='not-found-title'>Page not found!</h2>
-      <p>Please return to the home page and try again.</p>
+      <p className='not-found-msg'>Please return to the home page and try again.</p>
       <Link to='/'>
         <button className='return-btn'>Return Home</button>
       </Link>
