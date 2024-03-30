53bcda270e9ad0d6f76dbef32e7b14386092b471
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-03-29 19:43:27 -0500

    Restructure Api calls so they throw the errors to be displayed

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,32 +1,44 @@
 export function getUserInfo() {
   return fetch('https://be2310-social-media-aggregator-18a4f3a92617.herokuapp.com/api/v1/users/1')
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`Failed fetch`);
-    }
-  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Failed to fetch user info`);
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching user info:', error);
+      throw error; 
+    });
 }
 
 export function getCreators() {
   return fetch('https://be2310-social-media-aggregator-18a4f3a92617.herokuapp.com/api/v1/creators')
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`Failed fetch`);
-    }
-  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Failed to fetch creators`);
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching creators:', error);
+      throw error; 
+    });
 }
 
 export function getCreatorInfo(id: number) {
   return fetch(`https://be2310-social-media-aggregator-18a4f3a92617.herokuapp.com/api/v1/creators/${id}`)
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(`Failed fetch`);
-    }
-  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Failed to fetch creator info for ID ${id}`);
+      }
+    })
+    .catch(error => {
+      console.error(`Error fetching creator info for ID ${id}:`, error);
+      throw error; 
+    });
 }
