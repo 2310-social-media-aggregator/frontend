171e4b2e3b4294a7342e8a3763c45c34248a5313
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-04-16 22:49:37 -0500

    Header.test.tsx test suite

---
+++ src/Components/Header/Header.test.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Header from './Header';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+describe('Header component', () => {
+  const mockHandlePageSwitch = jest.fn();
+  const mockSetActiveTab = jest.fn();
+  const user = { name: 'Gavin' };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('homepage renders without crashing', () => {
+    render(
+      <Router>
+        <Header
+          handlePageSwitch={mockHandlePageSwitch}
+          activeTab="home" 
+          setActiveTab={mockSetActiveTab}
+          user={user}
+        />
+      </Router>
+    );
+  });
+
+  it('displays the platform name', () => {
+    const { getByText } = render(
+      <Router>
+        <Header
+          handlePageSwitch={mockHandlePageSwitch}
+          activeTab="home" 
+          setActiveTab={mockSetActiveTab}
+          user={user}
+        />
+      </Router>
+    );
+    expect(getByText('PLATFORM')).toBeInTheDocument();
+  });
+
+  it('displays the user name if user prop is provided', () => {
+    const { getByText } = render(
+      <Router>
+        <Header
+          handlePageSwitch={mockHandlePageSwitch}
+          activeTab="home" 
+          setActiveTab={mockSetActiveTab}
+          user={user}
+        />
+      </Router>
+    );
+    expect(getByText('Welcome, Gavin')).toBeInTheDocument();
+  });
+
+  it('renders correct active tab class based on activeTab prop', () => {
+    const { getByText } = render(
+      <Router>
+        <Header
+          handlePageSwitch={mockHandlePageSwitch}
+          activeTab="all"
+          setActiveTab={mockSetActiveTab}
+          user={user}
+        />
+      </Router>
+    );
+    const allCreatorsNavItem = getByText('All Creators');
+    expect(allCreatorsNavItem).toHaveClass('active');
+  });
+
+  it('calls handlePageSwitch and setActiveTab with correct tab when a nav item is clicked', () => {
+    const { getByText } = render(
+      <Router>
+        <Header
+          handlePageSwitch={mockHandlePageSwitch}
+          activeTab="home" 
+          setActiveTab={mockSetActiveTab}
+          user={user}
+        />
+      </Router>
+    );
+    const savedCreatorsNavItem = getByText('Saved Creators');
+    fireEvent.click(savedCreatorsNavItem);
+    expect(mockHandlePageSwitch).toHaveBeenCalledWith('saved');
+    expect(mockSetActiveTab).toHaveBeenCalledWith('saved');
+  });
+});
